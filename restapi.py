488826a530757badc920ea0fe31cbcf61e5baf84
falul <falul@walla.co.il>
2022-05-13 02:45:51 +0300

    Yolov5 support

--- restapi.py
+++ restapi.py
@@ -1,32 +1,31 @@
-"""
-Run a rest API exposing the yolov5s object detection model
-"""
 import argparse
 import io
+from io import BytesIO
+import base64
 from PIL import Image
 
 import torch
-from flask import Flask, request
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
+DETECTION_URL = "/analyze"
 
-DETECTION_URL = "/v1/object-detection/yolov5s"
+@app.route("/", methods=["GET"])
+def serve_index(): 
+    return render_template("index.html")
 
 
 @app.route(DETECTION_URL, methods=["POST"])
 def predict():
-    if not request.method == "POST":
-        return
-
+    print("image recieved")
     if request.files.get("image"):
         image_file = request.files["image"]
         image_bytes = image_file.read()
-
         img = Image.open(io.BytesIO(image_bytes))
-
-        results = model(img, size=640)
-        data = results.pandas().xyxy[0].to_json(orient="records")
-        return data
+        
+        results = model(img, size=640) # call the model
+        data = img_as_text(results)
+        return {'image_data': data, 'xyxy': results.pandas().xyxy[0].to_json(orient="records")}
 
 
 if __name__ == "__main__":
@@ -34,8 +33,20 @@ if __name__ == "__main__":
     parser.add_argument("--port", default=5000, type=int, help="port number")
     args = parser.parse_args()
 
-    model = torch.hub.load(
-        "ultralytics/yolov5", "yolov5s", pretrained=True, force_reload=True
-    ).autoshape()  # force_reload = recache latest code
-    model.eval()
-    app.run(host="0.0.0.0", port=args.port)  # debug=True causes Restarting with stat
+    model_path = "./custom.pt" 
+    # model_conf = 0.25 #@param {type:"slider", min:0, max:1, step:0.01}
+    # model_iou = .45 #@param {type:"slider", min:0, max:1, step:0.01}
+    model = torch.hub.load('ultralytics/yolov5', 'custom', path=model_path, force_reload=True)
+    # .autoshape()  # force_reload = recache latest code 
+
+def img_as_text(results):
+    results.imgs # array of original images (as np array) passed to model for inference
+    results.render()  # updates results.imgs with boxes and labels
+    
+    buffered = BytesIO()
+    im_base64 = Image.fromarray(results.imgs[0])
+    im_base64.save(buffered, format="JPEG")
+    return (base64.b64encode(buffered.getvalue()).decode('utf-8'))  # base64 encoded image with results
+    # model.eval()
+    
+app.run(host="0.0.0.0", port=args.port)  # debug=True causes Restarting with stat
--- tests/test_inference.py
+++ tests/test_inference.py
@@ -1,17 +1,39 @@
 import io
+
+from numpy import imag
 import torch
+import base64
+from io import BytesIO
 from PIL import Image
 
 # Model
-model = torch.hub.load("ultralytics/yolov5", "yolov5s", pretrained=True, force_reload=True)
+# model = torch.hub.load("ultralytics/yolov5", "yolov5s", pretrained=True, force_reload=True)
 
+model_path = "../custom.pt" #@param ["yolov5s", "yolov5/runs/train/exp/weights/best.pt", "custom.pt"] {allow-input: true}
+    # model_conf = 0.25 #@param {type:"slider", min:0, max:1, step:0.01}
+    # model_iou = .45 #@param {type:"slider", min:0, max:1, step:0.01}
+
+model = torch.hub.load('ultralytics/yolov5', 'custom', path=model_path, force_reload=True)
 # img = Image.open("zidane.jpg")  # PIL image direct open
 
 # Read from bytes as we do in app
-with open("zidane.jpg", "rb") as file:
+with open("35.jpg", "rb") as file:
     img_bytes = file.read()
 img = Image.open(io.BytesIO(img_bytes))
 
 results = model(img, size=640)  # includes NMS
+# image_as_string = base64.b64encode(cv2.imencode('.jpg', results)[1]).decode()
+#     # return image_as_string
+# print(image_as_string)
+# with open('./image_as_string', 'wb') as f:
+#                         f.write(image_as_string)
+# results.save()
 
-print(results.pandas().xyxy[0])
+results.imgs # array of original images (as np array) passed to model for inference
+results.render()  # updates results.imgs with boxes and labels
+for im in results.imgs:
+    buffered = BytesIO()
+    im_base64 = Image.fromarray(im)
+    im_base64.save(buffered, format="JPEG")
+    print(base64.b64encode(buffered.getvalue()).decode('utf-8'))  # base64 encoded image with results
+# print(results.pandas().xyxy[0])
